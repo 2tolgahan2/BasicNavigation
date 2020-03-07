2c037f580308763c5cd6add4f6a1dfba0359c5e9
Tolgahan Bardakcı <tolgahanbardakci@hotmail.com>
2020-03-08 00:36:05 +0300

    Added URL Validation & Check Field Case

--- BasicNavigation.py
+++ BasicNavigation.py
@@ -1,8 +1,23 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 21 Feb 2020
+
+@author: Tolgahan Bardakcı
+@author-email: bardakcitolgahan@gmail.com
+"""
+
+__version__ = "0.3"
+__author__ = "Tolgahan Bardakcı"
+__url__ = "https://github.com/2tolgahan2/BasicNavigation/blob/master/BasicNavigation.py"
+
+
 import unittest
 from selenium import webdriver
+import validators
 
-
-class PythonTest(unittest.TestCase):
+# Case: 1
+class NavigateTo(unittest.TestCase):
 
     @classmethod
     def setUpClass(inst):
@@ -10,7 +25,7 @@ class PythonTest(unittest.TestCase):
         chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
         chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en_UK'})
         inst.driver = webdriver.Chrome(options=chrome_options)
-        inst.driver.implicitly_wait(30)
+        inst.driver.implicitly_wait(20)
         inst.driver.maximize_window()
         # Navigate to the application home page
         inst.driver.get("https://www.python.org/")
@@ -18,24 +33,67 @@ class PythonTest(unittest.TestCase):
 
     def test_Navigate(self):
         # Check Navigation: "Start with Our Beginner's Guide"
-        # URL validation canbe added
-        
         self.navigate_link = self.driver.find_element_by_link_text('Start with our Beginner’s Guide')
         self.navigate_link.click()
 
-        #self.url = self.driver.current_url
+        # URL Validation
+        try:
+            self.url = "https://www.python.org/about/gettingstarted/"
+            self.validate = validators.url(self.url)
+            return self.validate.public
+        except:
+            assert False, "URL Validation is failed. Please check the url!"
+            
+        # Case:
         self.control = self.driver.title
-
         try:
             self.assertEqual(self.control, "Python For Beginners | Python.org")
         except AssertionError:
             assert False, "Navigate To: 'Start with our Beginner's Guide Page = Failed\n \t\tLook for: test_Navigate() "
         
+    @classmethod
+    def tearDownClass(inst):
+        inst.driver.quit()
+
+
+# Case: 2
+class CheckField(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(inst):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en_UK'})
+        inst.driver = webdriver.Chrome(options=chrome_options)
+        inst.driver.implicitly_wait(20)
+        inst.driver.maximize_window()
+        # Navigate to the application home page
+        inst.driver.get("https://www.python.org/about/gettingstarted/")
+        
+
+    def test_CheckField(self):
+        # Check Field & Data Type: Heading: "Python for Beginners"
+        
+        self.header = self.driver.find_element_by_xpath('//*[@id="content"]/div/section/article/header/h1')
+        self.control = self.header.text
+
+        try:
+            self.assertEqual(self.control, "Python For Beginners")
+        except AssertionError:
+            assert False, "Check Field & Data Type: Heading: 'Python for Beginners' = Failed\n \t\tLook for: test_CheckField() "
+
+
+    #def test_CheckField(self):
+        ## Check Validation: "..."
+
+
+    #def test_CheckContent(self):
+        ## Check Validation: "..."
 
     @classmethod
     def tearDownClass(inst):
-        # close the browser window
         inst.driver.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
