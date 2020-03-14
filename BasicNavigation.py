5da2feb3babd39f0217d63e8c50aa0f65b719c0d
Tolgahan Bardakcı <tolgahanbardakci@hotmail.com>
2020-03-15 00:44:10 +0300

    Changed the Test Structure

--- BasicNavigation.py
+++ BasicNavigation.py
@@ -4,10 +4,10 @@
 Created on 21 Feb 2020
 
 @author: Tolgahan Bardakcı
-@author-email: bardakcitolgahan@gmail.com
+@author-email: tolgahanbardakci@hotmail.com
 """
 
-__version__ = "0.3"
+__version__ = "0.4"
 __author__ = "Tolgahan Bardakcı"
 __url__ = "https://github.com/2tolgahan2/BasicNavigation/blob/master/BasicNavigation.py"
 
@@ -15,32 +15,37 @@ __url__ = "https://github.com/2tolgahan2/BasicNavigation/blob/master/BasicNaviga
 import unittest
 from selenium import webdriver
 import validators
+from selenium.webdriver.chrome.options import Options
 
-# Case: 1
-class NavigateTo(unittest.TestCase):
-
+# Setting up the Test Environment
+class BaseTestCase(unittest.TestCase):
     @classmethod
-    def setUpClass(inst):
+    def setUpClass(cls):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
         chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en_UK'})
-        inst.driver = webdriver.Chrome(options=chrome_options)
-        inst.driver.implicitly_wait(20)
-        inst.driver.maximize_window()
+        chrome_options.add_argument('--headless')
+        cls.driver = webdriver.Chrome(options=chrome_options)
+        cls.driver.implicitly_wait(5)
         # Navigate to the application home page
-        inst.driver.get("https://www.python.org/")
+        cls.driver.get("https://www.python.org/")
         
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
+
+# Test 1
+class NavigateTo(BaseTestCase):
     def test_Navigate(self):
         # Check Navigation: "Start with Our Beginner's Guide"
         self.navigate_link = self.driver.find_element_by_link_text('Start with our Beginner’s Guide')
         self.navigate_link.click()
 
-        # URL Validation
+        # URL Validation
         try:
             self.url = "https://www.python.org/about/gettingstarted/"
             self.validate = validators.url(self.url)
-            return self.validate.public
         except:
             assert False, "URL Validation is failed. Please check the url!"
             
@@ -50,30 +55,13 @@ class NavigateTo(unittest.TestCase):
             self.assertEqual(self.control, "Python For Beginners | Python.org")
         except AssertionError:
             assert False, "Navigate To: 'Start with our Beginner's Guide Page = Failed\n \t\tLook for: test_Navigate() "
-        
-    @classmethod
-    def tearDownClass(inst):
-        inst.driver.quit()
 
 
-# Case: 2
-class CheckField(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(inst):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en_UK'})
-        inst.driver = webdriver.Chrome(options=chrome_options)
-        inst.driver.implicitly_wait(20)
-        inst.driver.maximize_window()
-        # Navigate to the application home page
-        inst.driver.get("https://www.python.org/about/gettingstarted/")
-        
-
+# Test 2
+class CheckField(BaseTestCase):
     def test_CheckField(self):
-        # Check Field & Data Type: Heading: "Python for Beginners"
-        
+        # Check field for hedaer "Python For Beginners"
+        self.driver.get("https://www.python.org/about/gettingstarted/")
         self.header = self.driver.find_element_by_xpath('//*[@id="content"]/div/section/article/header/h1')
         self.control = self.header.text
 
@@ -83,16 +71,10 @@ class CheckField(unittest.TestCase):
             assert False, "Check Field & Data Type: Heading: 'Python for Beginners' = Failed\n \t\tLook for: test_CheckField() "
 
 
-    #def test_CheckField(self):
-        ## Check Validation: "..."
-
-
-    #def test_CheckContent(self):
-        ## Check Validation: "..."
-
-    @classmethod
-    def tearDownClass(inst):
-        inst.driver.quit()
+# Test 3
+#class CheckValidation(BaseTestCase):
+    #def test_CheckValidation(self):
+    #   ...
 
 
 if __name__ == '__main__':
